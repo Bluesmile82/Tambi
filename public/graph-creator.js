6764265232166adc4321d3a8a47c025a04d454aa
Al <pursuance@gmail.com>
2015-04-22 23:40:09 +0200

    almost

--- public/graph-creator.js
+++ public/graph-creator.js
@@ -1,9 +1,6 @@
 document.onload = (function(d3, saveAs, Blob, undefined){
   "use strict";
 
-$.getScript("show.js", function(){
-
-
   // define graphcreator object
   var GraphCreator = function(svg, nodes, edges){
     var thisGraph = this;
@@ -594,9 +591,11 @@ $.getScript("show.js", function(){
    var edges = [];//[{source: nodes[1], target: nodes[0]}];
 
 
-  GraphCreator.prototype.createIdea = function( title, x , y ) {
+  GraphCreator.prototype.createIdea = function() {
     var thisGraph = this,
-        d = {id: thisGraph.idct++, title: title, x: x , y: y};
+          state = thisGraph.state;
+    var xycoords = d3.mouse(thisGraph.svgG.node()),
+        d = {id: thisGraph.idct++, title: "new concept", x: xycoords[0], y: xycoords[1]};
     thisGraph.nodes.push(d);
     thisGraph.updateGraph();
   }
@@ -608,85 +607,4 @@ $.getScript("show.js", function(){
   var graph = new GraphCreator(svg, nodes, edges);
       graph.setIdCt(2);
   graph.updateGraph();
-
-
-// show.js
-
-
-
-function parseRelated(){
-  var url_related = 'http://es.wikipedia.org/w/api.php?action=query&prop=categories&format=json&titles=Albert%20Einstein&rnlimit=10&format=json&callback=?';
-  return ajaxCall(url_related)
-}
-
-function ajaxCall(url){
-  return $.ajax({
-      url: url,
-      dataType: 'json',
-    });
-}
-
-  function getIdeas(type){
-    var url = "";
-    switch(type) {
-    case 'random':
-      url = "http://es.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&iwurl=&redirects=&converttitles=&format=json&callback=?";
-        break;
-    case 'related':
-      parseRelated().done(function(data){console.log('parsed ', data)});
-      break;
-    }
-
-    return $.ajax({
-      url: url,
-      dataType: 'json',
-    });
-  }
-
-  function createIdeas(id, type){
-    getIdeas(type).done(function(data){
-
-      var translate = d3.select(id).attr('transform');
-      var parent = translate.match(/\((.+),(.+)\)/);
-      var parent_left = parent[1];
-      var parent_top = parent[2];
-      // $.each(data.query.random, function( index, value ) {
-      function sign(){ return Math.random() < 0.5 ? -1 : 1};
-      function delay(){ return (Math.random() * 5000) };
-      console.log('delay',delay() )
-      function left(){ return parseInt(parent_left) + (( Math.random() + 200) *  sign() ) };
-      var duration = 3000;
-      console.log('left',left() )
-
-      var  new_concept = d3.select("#playground").selectAll('div').data(data.query.random)
-                            .enter().append('div').attr('class', 'concept random')
-                            .style('top', function(data){ return sign() * 10 + 'px' })
-                            .style('left', function(data){ return left() +'px'  })
-                            .html(function(data) { return data.title; });
-
-      dead_concept = new_concept
-                  .transition().delay(delay).duration(duration).style({'opacity':'1'})
-                  .transition().duration(duration).style({'opacity':'0'})
-                  .duration(duration).attr('data-status','dead')
-                  .remove();
-       // });
-    });
-  }
-  // createIdeas("#initial","random");
-  d3.select('#random-button').on("click", function(){ createIdeas("#Laura","random")} );
-  d3.select('#related-button').on("click", function(){ createIdeas("#Laura","related")} );
- graph.createIdea("lalassssl", 500 , 600);
-
-
-  function parsePx(string){
-    return parseInt(string.replace('px',''));
-  }
-
-
-
-
-
-
-
-});
 })(window.d3, window.saveAs, window.Blob);
