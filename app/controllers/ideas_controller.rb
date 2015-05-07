2545f541ed0e811d5b60966d8b5777ecd8ddf4e3
Al <pursuance@gmail.com>
2015-05-07 02:31:33 +0200

    ideas without links loaded

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,7 +3,7 @@ class IdeasController < ApplicationController
   before_action :set_graph, only: [:index, :create, :edit, :update, :destroy]
 
   def index
-     @ideas = Idea.all
+     @ideas = @graph.ideas
   end
 
 
@@ -16,24 +16,18 @@ class IdeasController < ApplicationController
   def create
     concept = Concept.new(title: params[:concept][:title])
     concept.save
-    @idea = Idea.new(graph_id: @graph.id, concept_id: concept.id, id: idea_params[:id], x: idea_params[:x], y: idea_params[:y], font_size: idea_params[:font_size] )
-    if @idea.save
-      render nothing: true, status: 200
-    end
+    @idea = Idea.new( graph_id: @graph.id, concept_id: concept.id, id: idea_params[:id], x: idea_params[:x], y: idea_params[:y], font_size: idea_params[:font_size] )
+    @idea.save
   end
 
   # PATCH/PUT /ideas/1
   # PATCH/PUT /ideas/1.json
   def update
-    respond_to do |format|
-      if @idea.update(idea_params)
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
-        format.json { render :show, status: :ok, location: @idea }
-      else
-        format.html { render :edit }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
-    end
+    concept = Concept.new(title: params[:concept][:title])
+    concept.save
+    @idea.update( graph_id: @graph.id, concept_id: concept.id, id: idea_params[:id], x: idea_params[:x], y: idea_params[:y], font_size: idea_params[:font_size] )
+    @idea.save
+    render nothing: true, status: 200
   end
 
   # DELETE /ideas/1
---
+++ app/views/ideas/update.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @idea, :id, :x, :y, :font_size, :created_at, :updated_at
